8bfe77a90e1b23b12de8bbd0386387c86494a405
EswariMsundaram <eswarimsundaram@gmail.com>
2025-12-10 21:50:47 -0700

    Credentials added

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export const apiClient=axios.create({
     baseURL:import.meta.env.VITE_BACKEND_URL,
+    withCredentials: true
     // headers:{
     //     Authorization:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzljOWY3MmUwZGViYTJlZWFlZTJjZCIsInVzZXJuYW1lIjoidXNlcjMiLCJlbWFpbCI6InVzZXIzQHRlc3QuY29tIn0sImlhdCI6MTc2NTQxNzE3MiwiZXhwIjoxNzY1NTAzNTcyfQ.p2MLNPARIV8auXVnvq7x7GUEVkfa86P5S0TVcH0AY-M'
     // }
